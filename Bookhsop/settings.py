b1a5bde21b6967fc9760f1dbf91d013a6d976525
Zarnigor <165559679+zarnigordekhonova@users.noreply.github.com>
2024-05-12 12:38:46 +0500

    Update settings.py

--- Bookhsop/settings.py
+++ Bookhsop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)p4r0e_zs2=*3qvdo&-nwv+h(jzzp#4f=^e4xzaboi_*g!4$wf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['new-project-1-knxi.onrender.com']
 
 
 # Application definition
