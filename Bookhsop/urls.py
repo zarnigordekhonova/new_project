d0774ab790619a3456b75303254328f3e90fd56a
Zarnigor <165559679+zarnigordekhonova@users.noreply.github.com>
2024-05-12 12:54:41 +0500

    Update urls.py

--- Bookhsop/urls.py
+++ Bookhsop/urls.py
@@ -22,6 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Books1.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
-static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
